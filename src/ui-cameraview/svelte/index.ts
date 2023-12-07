f9c92a52952be4e1b91e7200c258397073b42946
farfromrefuge <>
2023-12-07 17:16:39 +0100

    chore: cleanup

--- src/ui-cameraview/svelte/index.ts
+++ src/ui-cameraview/svelte/index.ts
@@ -1,6 +1,5 @@
 import { ContentView, ItemEventData, View, profile } from '@nativescript/core';
 import { NativeViewElementNode, TemplateElement, ViewNode, createElement, registerNativeViewElement } from 'svelte-native/dom';
-import { flush } from 'svelte/internal';
 import { CameraView } from '..';
 
 export default class CameraViewViewElement extends NativeViewElementNode<CameraView> {
