f11ce073e19598c18111d0dc5cf8c66683b38002
Martin Guillon <dev@akylas.fr>
2026-04-28 10:06:08 +0200

    fix(ios): camera preview on simulator

---
+++ packages/ui-cameraview/nativescript.config.ts
@@ -0,0 +1,12 @@
+export default {
+    ios: {
+        SPMPackages: [
+            {
+                name: 'NextLevel',
+                libs: ['NextLevel'],
+                repositoryURL: 'https://github.com/akylas/NextLevel',
+                version: '~0.16.9'
+            }
+        ]
+    }
+};
